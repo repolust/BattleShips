f671fbf0ea000c6846e501a746708f612b22934e
repleb14 <repleb14@htlkaindorf.at>
2018-04-11 10:09:01 +0200

    Leonardos zweiter Code

--- BattleShips/src/battleships/BattleShips.java
+++ BattleShips/src/battleships/BattleShips.java
@@ -17,6 +17,7 @@ public class BattleShips {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Leonardos erste Zeile!");
+        System.out.println("Leonardos zweite Zeile!");
     }
     
 }
