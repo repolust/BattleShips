1de48e6809299afd051b9c616344ffb812db975f
repleb14 <repleb14@htlkaindorf.at>
2018-04-18 17:53:02 +0200

    StartMenu
    ColorDlg, PlayerDlg etc

--- BattleShips/src/GUI/StartMenuGUI.java
+++ BattleShips/src/GUI/StartMenuGUI.java
@@ -26,43 +26,16 @@ public class StartMenuGUI extends javax.swing.JFrame {
     /**
      * Creates new form StartMenuGUI
      */
-//    private Dimension screensize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
-//    private int hoehe = (int) screensize.getHeight();
-//    private int breite = (int) screensize.getWidth();
-////
-//    private final String pathBackground = System.getProperty("user.dir")
-//            + File.separator + "src"
-//            + File.separator + "bilder"
-//            + File.separator + "blasen.jpeg";
-//    private Image background;
+    private Dimension screensize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
+    private int hoehe = (int) screensize.getHeight();
+    private int breite = (int) screensize.getWidth();
+
 
     public StartMenuGUI() {
         initComponents();
-        //geil
-//        setEigenschaften();
-//        paint(getGraphics());
     }
 
-//    public void setEigenschaften() {
-//        Graphics g = getGraphics();
-//        this.setBounds(breite / 2 - breite / 4, hoehe / 2 - hoehe / 4, breite / 2, hoehe / 2);
-//        this.setResizable(false);
-//    }
 
-//    @Override
-//    public void paint(Graphics g) {
-//        int breite = getWidth();
-//        int hoehe = getHeight();
-//        try {
-//            super.paint(g); //To change body of generated methods, choose Tools | Templates.
-//            background = ImageIO.read(new File(pathBackground));
-//            g.drawImage(background, 0, 0, breite, hoehe, this);
-//            g.setFont(new Font("Georgia",Font.BOLD,60));
-//            
-//        } catch (IOException ex) {
-//            Logger.getLogger(StartMenuGUI.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-//    }
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -71,49 +44,55 @@ public class StartMenuGUI extends javax.swing.JFrame {
      */
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
-    private void initComponents()
-    {
+    private void initComponents() {
 
         jPanel2 = new javax.swing.JPanel();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jLabel1 = new javax.swing.JLabel();
+        btHilfe = new javax.swing.JButton();
+        btStarten = new javax.swing.JButton();
+        btBeenden = new javax.swing.JButton();
+        lbBild = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jPanel2.setLayout(null);
 
-        jButton1.setFont(new java.awt.Font("Tempus Sans ITC", 1, 48)); // NOI18N
-        jButton1.setText("Start");
-        jButton1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0), 2));
-        jButton1.setContentAreaFilled(false);
-        jButton1.addActionListener(new java.awt.event.ActionListener()
-        {
-            public void actionPerformed(java.awt.event.ActionEvent evt)
-            {
+        btHilfe.setFont(new java.awt.Font("Old English Text MT", 1, 36)); // NOI18N
+        btHilfe.setForeground(new java.awt.Color(255, 255, 255));
+        btHilfe.setText("Hilfe");
+        btHilfe.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(255, 255, 255), 4, true));
+        btHilfe.setContentAreaFilled(false);
+        jPanel2.add(btHilfe);
+        btHilfe.setBounds(40, 120, 220, 45);
+
+        btStarten.setFont(new java.awt.Font("Old English Text MT", 1, 36)); // NOI18N
+        btStarten.setForeground(new java.awt.Color(255, 255, 255));
+        btStarten.setText("Starten");
+        btStarten.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(255, 255, 255), 4));
+        btStarten.setContentAreaFilled(false);
+        btStarten.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
                 onStart(evt);
             }
         });
-        jPanel2.add(jButton1);
-        jButton1.setBounds(250, 80, 220, 60);
+        jPanel2.add(btStarten);
+        btStarten.setBounds(40, 60, 220, 45);
 
-        jButton2.setFont(new java.awt.Font("Tempus Sans ITC", 1, 48)); // NOI18N
-        jButton2.setText("Beenden");
-        jButton2.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0), 2));
-        jButton2.setContentAreaFilled(false);
-        jButton2.addActionListener(new java.awt.event.ActionListener()
-        {
-            public void actionPerformed(java.awt.event.ActionEvent evt)
-            {
+        btBeenden.setFont(new java.awt.Font("Old English Text MT", 1, 36)); // NOI18N
+        btBeenden.setForeground(new java.awt.Color(255, 255, 255));
+        btBeenden.setText("Beenden");
+        btBeenden.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(255, 255, 255), 4));
+        btBeenden.setContentAreaFilled(false);
+        btBeenden.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
                 onBeenden(evt);
             }
         });
-        jPanel2.add(jButton2);
-        jButton2.setBounds(250, 190, 220, 90);
+        jPanel2.add(btBeenden);
+        btBeenden.setBounds(40, 180, 220, 45);
 
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Bilder/blasen.jpeg"))); // NOI18N
-        jPanel2.add(jLabel1);
-        jLabel1.setBounds(0, 0, 690, 390);
+        lbBild.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Bilder/blasen.jpeg"))); // NOI18N
+        jPanel2.add(lbBild);
+        lbBild.setBounds(0, 0, 690, 390);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -131,12 +110,13 @@ public class StartMenuGUI extends javax.swing.JFrame {
 
     private void onStart(java.awt.event.ActionEvent evt)//GEN-FIRST:event_onStart
     {//GEN-HEADEREND:event_onStart
-       JOptionPane.showMessageDialog(null, "Haha Nigga");
+       PlayerDlg pldg = new PlayerDlg();
+       pldg.setVisible(true);
     }//GEN-LAST:event_onStart
 
     private void onBeenden(java.awt.event.ActionEvent evt)//GEN-FIRST:event_onBeenden
     {//GEN-HEADEREND:event_onBeenden
-        JOptionPane.showMessageDialog(null, "Tschau Kakao");
+        dispose();
     }//GEN-LAST:event_onBeenden
 
     /**
@@ -176,9 +156,10 @@ public class StartMenuGUI extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JLabel jLabel1;
+    private javax.swing.JButton btBeenden;
+    private javax.swing.JButton btHilfe;
+    private javax.swing.JButton btStarten;
     private javax.swing.JPanel jPanel2;
+    private javax.swing.JLabel lbBild;
     // End of variables declaration//GEN-END:variables
 }
